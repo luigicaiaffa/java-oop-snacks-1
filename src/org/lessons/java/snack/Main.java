0bdac6771e7442e9d916d5755fe8dc0a44b29fdf
luigicaiaffa <luigicaiaffa8@gmail.com>
2025-03-07 14:04:56 +0100

    Snack 1

---
+++ src/org/lessons/java/snack/Main.java
@@ -0,0 +1,9 @@
+package org.lessons.java.snack;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student("Mario", "Rossi", 25);
+
+        student.getStudent();
+    }
+}
--- src/org/lessons/java/snack/Student.java
+++ src/org/lessons/java/snack/Student.java
@@ -5,5 +5,17 @@ public class Student {
     // # Variabili di istanza
     private String name;
     private String surname;
-    private String age;
+    private int age;
+
+    // # Costruttore
+    public Student(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    // # Metodi
+    public void getStudent() {
+        System.out.println("Studente: " + this.name + " " + this.surname + ", " + this.age + " anni");
+    }
 }
